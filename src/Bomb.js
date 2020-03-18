3aeccb40acc03540e7365cbde05b1412817397ee
Gilblasse <gilblasse@gmail.com>
2020-03-18 18:17:06 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,9 @@
 // your Bomb code here!
+import React, { Component } from "react";
+
+export default class Bomb extends Component {
+  state = { secondsLeft: this.props.initialCount };
+  render() {
+    return <div> textInComponent </div>;
+  }
+}
